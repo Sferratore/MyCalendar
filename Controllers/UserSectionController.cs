39519af90d06664dfb2aca694fbc57cd15afe194
Sferratore <sferratore.francescopaolo00@gmail.com>
2023-08-05 11:50:20 +0200

    Added graphics on main user homepage. Added annotation creation page. Need to modify TaskReminder class name and add foreign key to creation user.

--- Controllers/UserSectionController.cs
+++ Controllers/UserSectionController.cs
@@ -1,12 +1,44 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyCalendar.Data;
+using MyCalendar.Models;
 
 namespace MyCalendar.Controllers
 {
     public class UserSectionController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public UserSectionController(ApplicationDbContext db)
+        {
+            this._db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Calendar()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(TaskReminder formTask)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.TaskReminders.Add(formTask);
+                _db.SaveChanges();
+                TempData["success"] = "Your annotation has been created successfully.";
+                return RedirectToAction("Index");
+            }
+            return View(formTask);
         }
     }
 }
