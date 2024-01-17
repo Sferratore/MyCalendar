7d5690dcbb2cd05230912c965a334ff7545e3187
Francesco Paolo Sferratore <sferratore.francescopaolo00@gmail.com>
2024-01-17 14:09:22 +0100

    Commented ErrorViewModel.cs

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,9 +1,14 @@
-namespace MyCalendar.Models
+﻿namespace MyCalendar.Models
 {
+    // Represents the error details that can be shown in the view
     public class ErrorViewModel
     {
+        // The unique ID associated with the HTTP request that generated the error
         public string? RequestId { get; set; }
 
+        // A boolean property to determine whether to show the RequestId in the view.
+        // Returns true if the RequestId is not null or empty, indicating an error ID is present.
+        //Can be modified to adapt to visibility-of-error rules.
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
