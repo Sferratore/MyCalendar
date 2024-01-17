99c715dc59462aa4b1392b5bb1b8b0e9b3daee76
Francesco Paolo Sferratore <sferratore.francescopaolo00@gmail.com>
2024-01-17 14:04:22 +0100

    Commented User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -2,25 +2,34 @@
 
 namespace MyCalendar.Models
 {
+    // Represents a user in the system
     public class User
     {
+        // Unique identifier for the user
         [Key]
         public int IdUser { get; set; }
+
+        // Username of the user, required and restricted to a maximum length of 40 characters
         [Required]
         [StringLength(40)]
         public string Username { get; set; }
+
+        // Email address of the user, required and limited to a maximum length of 250 characters
         [StringLength(250)]
         [Required]
         public string Email { get; set; }
 
+        // Used for confirming the email, must match the Email field. The "Compare" DataAnnotation checks if this field matches with its first argument.
         [Compare("Email", ErrorMessage = "The email and confirmation email do not match.")]
         public string ConfirmEmail { get; set; }
 
+        // Password for the user, required with a minimum length of 6 characters and maximum length of 250 characters
         [StringLength(250)]
         [MinLength(6)]
         [Required]
         public string Password { get; set; }
 
+        // Date when the user subscribed, required field
         [Required]
         public DateTime SubscriptionDate { get; set; }
     }
