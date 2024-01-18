5274332a4f0cc2f629c93eb6a29103ecf60a5c7b
Francesco Paolo Sferratore <sferratore.francescopaolo00@gmail.com>
2024-01-18 19:01:30 +0100

    Created Migration for Db. Now User has dropped ConfirmationEmail column.

--- Migrations/20240118175904_corrected-user.Designer.cs
+++ Migrations/20240118175904_corrected-user.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyCalendar.Data;
 
@@ -11,9 +12,10 @@ using MyCalendar.Data;
 namespace MyCalendar.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240118175904_corrected-user")]
+    partial class correcteduser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20240118175904_corrected-user.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyCalendar.Migrations
+{
+    public partial class correcteduser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConfirmEmail",
+                table: "Users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ConfirmEmail",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
